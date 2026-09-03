6d56226d57f70eb06d88bd7ec5321ba26dd9cec8
Akash <ak668463@gmail.com>
2026-09-04 02:40:54 +0530

    Added CreateProductEndpoint and used BuildingBlocks for CQRS

--- Src/Services/Catalog/Catalog.API/Program.cs
+++ Src/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//Add Services to the container
+// Add services to the container.
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 
 var app = builder.Build();
 
-//configure the HTTP request pipline.
+// Configure the HTTP request pipeline.
+app.MapCarter();
 
 app.Run();
