23ffa3e8baae82bc0a088c5e3e1b9f52d2bc7f4f
Akash <ak668463@gmail.com>
2026-09-09 01:25:56 +0530

    Implementing FluentValidation for Catalog API

--- Src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
+++ Src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductHandler.cs
@@ -1,7 +1,4 @@
-﻿
-using FluentValidation;
-
-namespace Catalog.API.Products.DeleteProduct;
+﻿namespace Catalog.API.Products.DeleteProduct;
 
 public record DeleteProductCommand(Guid Id) : ICommand<DeleteProductResult>;
 public record DeleteProductResult(bool IsSuccess);
--- Src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ Src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,7 +1,4 @@
-﻿
-using FluentValidation;
-
-namespace Catalog.API.Products.UpdateProduct;
+﻿namespace Catalog.API.Products.UpdateProduct;
 
 public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<UpdateProductResult>;
