19f5e7a1d291ec26aed18e74652a1160cf1931e3
Akash <ak668463@gmail.com>
2026-09-10 02:44:17 +0530

    Implementing Pagination fro GetProducts

--- Src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ Src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -9,3 +9,4 @@ global using BuildingBlocks.Behaviors;
 global using Catalog.API.Exceptions;
 global using BuildingBlocks.Exceptions.Handler;
 global using Catalog.API.Data;
+global using Marten.Pagination;
